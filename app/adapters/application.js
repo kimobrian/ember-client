c38bd35786cc6352402fe5d91ba4be4772764cf0
kimobrian <brian.kimokoti@andela.com>
2018-06-30 13:54:51 +0300

    chore(model): Create model for an assessment

--- app/adapters/application.js
+++ app/adapters/application.js
@@ -1,5 +1,5 @@
 import DS from 'ember-data';
-import config from './config/environment';
+import config from '../config/environment';
 
 export default DS.RESTAdapter.extend({
   host: config.API_URL,
